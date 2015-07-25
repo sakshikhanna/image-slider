f56ee8a4aab3398038f991a4d71441c6f35c5940
Sakshi <khanna.sakshi09@gmail.com>
2015-07-26 00:48:48 +0530

    deleted example

--- Example/js/slider.js
+++
@@ -1,8 +0,0 @@
- window.currentImageSlider = new ImageSlider( '.image-container-list', '.list-item', 500 );
-    $( '.crs-image-section-next' ).on( 'click', function() {
-        currentImageSlider.next();
-    });
-
-    $( '.crs-image-section-prev' ).on( 'click', function() {
-        currentImageSlider.prev();
-    });
