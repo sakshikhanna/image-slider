a6536dfbc44090444d939fa43c32e0c7c2e9534f
Sakshi <khanna.sakshi09@gmail.com>
2015-07-29 20:57:42 +0530

    removed Bug

--- js/slider.js
+++ js/slider.js
@@ -1,15 +1,15 @@
  // window.currentImageSlider = new ImageSlider( '.image-container-list', '.list-item', 500 );
  var defaults = {
- 	parentClass = '.image-container-list',
- 	childClass = '.list-item',
- 	speed = 500
+ 	parentClass : '.image-container-list',
+ 	childClass : '.list-item',
+ 	speed : 500
  }
 
   window.currentImageSlider = new ImageSlider( defaults );
-    $( '.crs-image-section-next' ).on( 'click', function() {
+    $( '.next-arrow' ).on( 'click', function() {
         currentImageSlider.next();
     });
 
-    $( '.crs-image-section-prev' ).on( 'click', function() {
+    $( '.prev-arrow' ).on( 'click', function() {
         currentImageSlider.prev();
     });
